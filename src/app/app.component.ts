87844b22f521b2fcbd1c088204fd4cac49fefa53
oscarkuery <oscar@kuery.com.co>
2023-09-11 16:37:21 -0500

    s
    Se suben cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,90 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba-proyecto-mutante';
+  secuenciadna: string ='';
+  myForm:FormGroup;
+  mutante: boolean = false;
+  nomutante: boolean = false;
+
+  dnaFormControl = new FormControl('', [
+    Validators.required, // Campo requerido
+    Validators.pattern(/^[ACGTacgt\n]+$/), // Patrón para permitir solo A, C, G, T y saltos de línea
+  ]);
+
+  constructor(private router: Router,private fb: FormBuilder){
+
+    this.myForm= this.fb.group({
+          'miTextarea': new FormControl('', [
+            Validators.required, // Campo requerido
+            Validators.pattern(/^[ACGTacgt\n]+$/), // Patrón para permitir solo A, C, G, T y saltos de línea
+          ])
+    });
+
+  }
+
+  detectarmutante(){
+
+    let valor = this.myForm.value.miTextarea;
+    this.isMutant(valor);
+    this.mutante
+
+  }
+
+
+  isMutant(dna: any) {
+    const sequencesToCheck = ['AAAA', 'TTTT', 'CCCC', 'GGGG'];
+  
+    for (let i = 0; i < dna.length; i++) {
+      for (let j = 0; j < dna[i].length; j++) {
+        // Verificar horizontalmente
+        for (const sequence of sequencesToCheck) {
+          if (dna[i].includes(sequence)) {
+            return true;
+          }
+        }
+  
+        // Verificar verticalmente
+        let verticalSequence = '';
+        for (let k = 0; k < dna.length; k++) {
+          verticalSequence += dna[k][j];
+        }
+        for (const sequence of sequencesToCheck) {
+          if (verticalSequence.includes(sequence)) {
+            return true;
+          }
+        }
+  
+        // Verificar diagonalmente (de izquierda a derecha)
+        let diagonalSequence = '';
+        for (let k = 0; k < dna.length; k++) {
+          if (i + k < dna.length && j + k < dna[i].length) {
+            diagonalSequence += dna[i + k][j + k];
+          }
+        }
+        for (const sequence of sequencesToCheck) {
+          if (diagonalSequence.includes(sequence)) {
+            return true;
+          }
+        }
+  
+        // Verificar diagonalmente (de derecha a izquierda)
+        let antiDiagonalSequence = '';
+        for (let k = 0; k < dna.length; k++) {
+          if (i + k < dna.length && j - k >= 0) {
+            antiDiagonalSequence += dna[i + k][j - k];
+          }
+        }
+        for (const sequence of sequencesToCheck) {
+          if (antiDiagonalSequence.includes(sequence)) {
+            return true;
+          }
+        }
+      }
+    }
+  
+    return false;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule, 
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
